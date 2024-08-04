4d60ca1eae5d8496c56c207a459b706dc96ff061
hafiz-bs23 <hafizur.bs23@gmail.com>
2024-08-04 16:19:24 +0600

    Add failing case to see the workflow

--- backend/test_main.py
+++ backend/test_main.py
@@ -13,3 +13,8 @@ def test_app_sayOMG():
     response = client.get("/omg")
     assert response.status_code == 200
     assert response.json() == {"data": "Oh my F***ing god it is working"}
+    
+def test_app_sayOMG_Not_Found():
+    response = client.get("/omg")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Not Found"}
